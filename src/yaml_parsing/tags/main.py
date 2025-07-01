2987149583ad35a93ec1106d508834f9bef6cf39
Bill Wallis <bill@tasman.ai>
2025-07-01 06:51:23 +0100

    feat: add YAML parser examples

---
+++ src/yaml_parsing/tags/main.py
@@ -0,0 +1,91 @@
+"""
+PyYAML adheres to the YAML 1.1 specification.
+ruyaml adheres to the YAML 1.2 specification by default (and supports 1.1).
+
+There's a big difference between the two specs for boolean values:
+
+- https://yaml.org/type/bool.html
+- https://yaml.org/spec/1.2.2/#10212-boolean
+"""
+
+import dataclasses
+import pathlib
+from collections.abc import Callable
+
+import ruyaml
+import yaml  # pyyaml
+
+GREEN = "\033[1;32m"
+BLUE = "\033[1;34m"
+RESET = "\033[0m"
+HERE = pathlib.Path(__file__).parent
+
+
+def colour(text: str, colour_: str) -> str:
+    """
+    Return the text in the given colour.
+    """
+
+    return f"{colour_}{text}{RESET}"
+
+
+def print_values(content: str, loader: Callable) -> None:
+    """
+    Print the values of a YAML document using the specified loader.
+    """
+
+    for document in loader(content):
+        for item in document.items():
+            print(f"  {item[0]}:")
+            for k, v in item[1].items():
+                col = BLUE if isinstance(v, bool) else GREEN
+                print(f"    {k}: {colour(v, col)}")
+
+
+def bools_example(content: str):
+    """
+    Compare boolean value serialisation between ruyaml and PyYAML.
+    """
+
+    print("\nUsing ruyaml:")
+    print_values(content, ruyaml.safe_load_all)
+
+    print("\nUsing PyYAML:")
+    print_values(content, yaml.safe_load_all)
+
+
+@dataclasses.dataclass
+class Model:
+    name: str
+    archived: bool
+    columns: list[str]
+
+
+def python_example__simple(content: str):
+    """
+    Serialise YAML content to a Python object.
+    """
+
+    print("\nUsing ruyaml:")
+    print("   ", ruyaml.load(content, Loader=ruyaml.Loader))
+
+    print("\nUsing PyYaml:")
+    print("   ", yaml.load(content, Loader=yaml.Loader))  # noqa: S506
+
+
+def python_example__complex():
+    """
+    Outsource to:
+
+    - https://github.com/TasmanAnalytics/openapi-sample/tree/main/src/catalogue
+    """
+
+
+def main():
+    bools_example((HERE / "bools.yaml").read_text())
+    # bools_example((HERE / "bools-tagged.yaml").read_text())
+    # python_example__simple((HERE / "python-tag.yaml").read_text())
+
+
+if __name__ == "__main__":
+    main()
