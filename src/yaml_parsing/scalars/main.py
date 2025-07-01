fbb273f5c4748a0ca93065234f67eca0019a4291
Bill <Bill@TheWallisFamily.co.uk>
2025-07-01 06:52:23 +0100

    feat: add YAML parser examples (#2)

---
+++ src/yaml_parsing/scalars/main.py
@@ -0,0 +1,19 @@
+import pathlib
+
+import yaml
+
+HERE = pathlib.Path(__file__).parent
+
+
+def main():
+    raw_content = (HERE / "ahoy.yaml").read_text(encoding="utf-8")
+    # raw_content = (HERE / "real-example.yaml").read_text(encoding="utf-8")
+    contents = yaml.safe_load_all(raw_content)
+    for document in contents:
+        for item in document.items():
+            print(42 * "-", "\n", item, sep="")
+            print(item[1])
+
+
+if __name__ == "__main__":
+    main()
