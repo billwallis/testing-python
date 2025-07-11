820b182abbdda0ca28f1b2824ec23f4ff4c1c5dd
Bill Wallis <bill@tasman.ai>
2025-07-11 16:32:45 +0100

    feat: add set and omap example

--- src/yaml_parsing/tags/main.py
+++ src/yaml_parsing/tags/main.py
@@ -1,4 +1,8 @@
 """
+Let's talk about tags:
+
+- https://yaml.org/spec/1.2.2/#24-tags
+
 PyYAML adheres to the YAML 1.1 specification.
 ruamel.yaml adheres to the YAML 1.2 specification by default (and supports 1.1).
 
@@ -17,6 +21,7 @@ import yaml  # pyyaml
 
 GREEN = "\033[1;32m"
 BLUE = "\033[1;34m"
+PURPLE = "\033[1;35m"
 RESET = "\033[0m"
 HERE = pathlib.Path(__file__).parent
 
@@ -29,7 +34,11 @@ def colour(text: str, colour_: str) -> str:
     return f"{colour_}{text}{RESET}"
 
 
-def print_values(content: str, loader: Callable) -> None:
+USING_RUAMEL = colour("\nUsing ruamel.yaml", PURPLE)
+USING_PYYAML = colour("\nUsing PyYAML", PURPLE)
+
+
+def _print_values(content: str, loader: Callable) -> None:
     """
     Print the values of a YAML document using the specified loader.
     """
@@ -47,11 +56,11 @@ def bools_example(content: str):
     Compare boolean value serialisation between ruamel.yaml and PyYAML.
     """
 
-    print("\nUsing ruamel.yaml:")
-    print_values(content, ruyaml.YAML(typ="safe", pure=True).load_all)
+    print(USING_RUAMEL)
+    _print_values(content, ruyaml.YAML(typ="safe", pure=True).load_all)
 
-    print("\nUsing PyYAML:")
-    print_values(content, yaml.safe_load_all)
+    print(USING_PYYAML)
+    _print_values(content, yaml.safe_load_all)
 
 
 def versioned_example(content: str):
@@ -59,11 +68,23 @@ def versioned_example(content: str):
     Compare versioned YAML tags between ruamel.yaml and PyYAML.
     """
 
-    print("\nUsing ruamel.yaml:")
-    print_values(content, ruyaml.YAML(typ="safe", pure=True).load_all)
+    print(USING_RUAMEL)
+    _print_values(content, ruyaml.YAML(typ="safe", pure=True).load_all)
 
-    print("\nUsing PyYAML:")
-    print_values(content, yaml.safe_load_all)
+    print(USING_PYYAML)
+    _print_values(content, yaml.safe_load_all)
+
+
+def set_and_omap_example(content: str):
+    """
+    Compare sets and ordered map YAML tags between ruamel.yaml and PyYAML.
+    """
+
+    print(USING_RUAMEL)
+    print(" " * 3, next(ruyaml.YAML(typ="safe", pure=True).load_all(content)))
+
+    print(USING_PYYAML)
+    print(" " * 3, next(yaml.safe_load_all(content)))
 
 
 @dataclasses.dataclass
@@ -78,10 +99,10 @@ def python_example__simple(content: str):
     Serialise YAML content to a Python object.
     """
 
-    print("\nUsing ruamel.yaml:")
+    print(USING_RUAMEL)
     print("   ", ruyaml.YAML(typ="unsafe").load(content))
 
-    print("\nUsing PyYaml:")
+    print(USING_PYYAML)
     print("   ", yaml.load(content, Loader=yaml.Loader))  # noqa: S506
 
 
@@ -98,6 +119,7 @@ def main():
     # bools_example((HERE / "bools-tagged.yaml").read_text())
     # versioned_example((HERE / "versioned-1.1.yaml").read_text())
     # versioned_example((HERE / "versioned-1.2.yaml").read_text())
+    # set_and_omap_example((HERE / "set-and-omap.yaml").read_text())
     # python_example__simple((HERE / "python-tag.yaml").read_text())
 
 
