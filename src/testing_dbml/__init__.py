f755c0489060dbb8478fcdcea466619fbf87016f
Bill <bill@thewallisfamily.co.uk>
2025-07-20 10:54:29 +0100

    feat: add DBML example

---
+++ src/testing_dbml/__init__.py
@@ -0,0 +1,21 @@
+"""
+Holistics are a BI and data company:
+
+- https://www.holistics.io/
+
+They have several products, including a DSL specification for defining
+database structures called DBML (database markup language):
+
+- https://dbml.dbdiagram.io/home
+- https://github.com/holistics/dbml/
+
+Some of their other products, such as dbdiagram.io and RunSQL, use DBML
+as part of their functionality:
+
+- https://dbdiagram.io/
+- https://runsql.com/
+
+The DBML specification is available at:
+
+- https://dbml.dbdiagram.io/docs
+"""
